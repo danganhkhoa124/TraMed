fa70fec4ed2ad1cf696dabc45c65747e94c881b0
danganhkhoa124 <danganhkhoa124@gmail.com>
2025-03-30 09:53:24 +0700

    build project

---
+++ src/test/java/tramed/TraMedApplicationTests.java
@@ -0,0 +1,13 @@
+package tramed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
