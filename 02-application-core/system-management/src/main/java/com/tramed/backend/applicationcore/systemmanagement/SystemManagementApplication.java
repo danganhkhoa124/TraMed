75e700f58a4b52cb7c193166f52e91179b03e189
danganhkhoa124 <danganhkhoa124@gmail.com>
2025-04-15 23:29:07 +0700

    connect subproject for call api get

--- 02-application-core/system-management/src/main/java/com/tramed/backend/applicationcore/systemmanagement/SystemManagementApplication.java
+++ 02-application-core/system-management/src/main/java/com/tramed/backend/applicationcore/systemmanagement/SystemManagementApplication.java
@@ -1,9 +1,13 @@
 package com.tramed.backend.applicationcore.systemmanagement;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.tramed.backend.infrastructure.mybatis")
+@MapperScan(basePackages = "com.tramed.backend.infrastructure.mybatis.mapper")
 public class SystemManagementApplication {
   public static void main(String[] args) {
     SpringApplication.run(SystemManagementApplication.class, args);
--- 02-application-core/system-management/src/main/java/com/tramed/backend/applicationcore/systemmanagement/service/notification/NotificationService.java
+++ 02-application-core/system-management/src/main/java/com/tramed/backend/applicationcore/systemmanagement/service/notification/NotificationService.java
@@ -1,27 +1,18 @@
 package com.tramed.backend.applicationcore.systemmanagement.service.notification;
 
-import com.tramed.backend.core.base.model.common.Locale;
-import com.tramed.backend.core.base.model.common.UserId;
-import com.tramed.backend.core.base.model.notification.NotificationContentId;
-import com.tramed.backend.core.base.model.notification.NotificationId;
 import com.tramed.backend.core.base.model.notification.NotificationQueryResult;
-import java.time.Instant;
+import com.tramed.backend.infrastructure.mybatis.repository.NotificationContentRepository;
 import java.util.List;
-import java.util.UUID;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class NotificationService {
+
+  private final NotificationContentRepository notificationContentRepository;
+
   public List<NotificationQueryResult> fetchNotification() {
-    return List.of(
-        new NotificationQueryResult(
-            new NotificationContentId(UUID.randomUUID()),
-            new NotificationId(UUID.randomUUID()),
-            "test",
-            Locale.VI_VN,
-            new UserId(UUID.randomUUID()),
-            Instant.now()));
+    return notificationContentRepository.fetchNotificationContentByLocale("");
   }
 }
