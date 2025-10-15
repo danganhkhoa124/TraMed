efc0ed402a2c74522e3f9f7c29bc65b15f1d0601
Anh_Khoa <68287539+danganhkhoa124@users.noreply.github.com>
2025-10-15 20:31:00 +0700

    test: isolate integration tests and simplify context load (#5)
    * test: isolate integration tests and simplify context load
    
    * fix: prevent integration tests from running in unit task
    
    * 20251015 - fix format

--- 01-tramed-presentation/web-api/src/integrationTest/java/com/tramed/backend/presentation/webapi/support/BaseIntegrationTest.java
+++ 01-tramed-presentation/web-api/src/integrationTest/java/com/tramed/backend/presentation/webapi/support/BaseIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.StreamUtils;
 
+@IntegrationTest
 @SpringBootTest(classes = WebApiApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
