7f98c00f9f183e3de7523645cb7b33cc17dcf32f
danganhkhoa124 <danganhkhoa124@gmail.com>
2025-10-31 16:39:11 +0700

    20251031 remove turn off database because unuse jpa starter for all subproject

--- 00-tramed-core/base/src/test/java/com/tramed/backend/core/base/BaseApplicationTests.java
+++ 00-tramed-core/base/src/test/java/com/tramed/backend/core/base/BaseApplicationTests.java
@@ -1,14 +1,9 @@
 package com.tramed.backend.core.base;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = BaseApplication.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // turn off database
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) // turn off database
 class BaseApplicationTests {
 
   @Test
