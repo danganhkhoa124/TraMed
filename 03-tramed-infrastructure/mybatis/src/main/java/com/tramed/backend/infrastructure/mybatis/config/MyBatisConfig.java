0bd27ec5ee83163f45c3efb94c4aa7b58d8f5497
Anh_Khoa <68287539+danganhkhoa124@users.noreply.github.com>
2025-10-31 16:50:42 +0700

    20251031 - Align docker Postgres defaults with shared test settings (#9)
    * Align docker postgres env with test defaults
    
    * 20251031 refactor
    
    * 20251031 remove config unused in mybatis
    
    * 20251031 remove turn off database because unuse jpa starter for all subproject

--- 03-tramed-infrastructure/mybatis/src/main/java/com/tramed/backend/infrastructure/mybatis/config/MyBatisConfig.java
+++ 03-tramed-infrastructure/mybatis/src/main/java/com/tramed/backend/infrastructure/mybatis/config/MyBatisConfig.java
@@ -9,32 +9,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@MapperScan(
-    basePackages = "com.tramed.backend.infrastructure.mybatis.mapper",
-    sqlSessionFactoryRef = "sqlSessionFactory")
-public class MyBastisConfig {
-
-  @Bean
-  @ConfigurationProperties("spring.datasource")
-  public DataSourceProperties dataSourceProperties() {
-    return new DataSourceProperties();
-  }
-
-  @Bean(name = "dataSource")
-  public DataSource dataSource() {
-    return dataSourceProperties().initializeDataSourceBuilder().build();
-  }
-
+@MapperScan(basePackages = "com.tramed.backend.infrastructure.mybatis.mapper")
+public class MyBatisConfig {
   @Bean
   @Primary
   public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource)
@@ -53,11 +35,5 @@ public class MyBastisConfig {
     sqlSessionFactoryBean.setMapperLocations(
         new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/**/*.xml"));
     return sqlSessionFactoryBean.getObject();
-  }
-
-  @Bean
-  @Primary
-  public PlatformTransactionManager transactionManager(DataSource dataSource) {
-    return new DataSourceTransactionManager(dataSource);
   }
 }
